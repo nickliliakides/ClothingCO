efd1113ad975807fa041d816cd218b104d360066
Nick Liliakides <nick.liliakides@gmail.com>
2020-08-19 02:55:46 +0100

    Folder structure changed Express server added to help handling stripe payment error corrected

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(cors());
 
-if (process.env.NODE_ENV !== 'production') {
+if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, 'client/build')));
 
   app.get('*', function (req, res) {
